3d4e8731112e913dbb64a8fcd7c224a3eca4e317
Kayky Nery <kaykynery38@gmail.com>
2025-08-15 22:29:20 -0300

    Updated About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -114,7 +114,7 @@ const About: React.FC = () => {
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
               <div>
                 <img 
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrfE-5uk6MTtPlRm0iRFtyNcCm5gJCIKeUqQ&s"
+                  src="https://madezanmadeiras.com.br/files/imagem/1293928295755e71c1de185.36080464.jpg"
                   alt="Equipe da fazenda"
                   className="w-full h-96 object-cover rounded-xl shadow-lg"
                 />
