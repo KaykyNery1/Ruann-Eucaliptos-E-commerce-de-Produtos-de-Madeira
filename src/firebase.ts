45be2cc2a278dc5e3e3672226404fe55b3ba91a6
Kayky Nery <kaykynery38@gmail.com>
2025-08-18 00:48:21 -0300

    Updated firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,20 @@
+// src/firebase.ts
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+// Sua configuração do Firebase (copie do console)
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_BUCKET",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyDfICHDvsembV5MWDNcQ1sBLh8Z5wnjwf0",
+  authDomain: "ruann-eucaliptos-3d605.firebaseapp.com",
+  projectId: "ruann-eucaliptos-3d605",
+  storageBucket: "ruann-eucaliptos-3d605.appspot.com",
+  messagingSenderId: "539829166736",
+  appId: "1:539829166736:web:748b14b536faf03e7aae15"
 };
 
+// Inicializa o app
 const app = initializeApp(firebaseConfig);
+
+// Inicializa o Auth
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
