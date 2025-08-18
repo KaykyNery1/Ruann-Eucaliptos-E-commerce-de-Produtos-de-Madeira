7d62ce927ad2cfa973888ae777f51d3dc31c5246
Kayky Nery <kaykynery38@gmail.com>
2025-08-18 00:43:53 -0300

    Updated firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,15 @@
-// src/firebase.ts
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDfICHDvsembV5MWDNcQ1sBLh8Z5wnjwf0",
-  authDomain: "ruann-eucaliptos-3d605.firebaseapp.com",
-  projectId: "ruann-eucaliptos-3d605",
-  storageBucket: "ruann-eucaliptos-3d605.firebasestorage.app",
-  messagingSenderId: "539829166736",
-  appId: "1:539829166736:web:748b14b536faf03e7aae15"
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_AUTH_DOMAIN",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_BUCKET",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider();
-
-export { auth, googleProvider };
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
