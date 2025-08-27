233697f3e32eb5820bc72ea356657cedb1d4b8e5
Kayky Nery <kaykynery38@gmail.com>
2025-08-27 17:15:14 -0300

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -164,7 +164,7 @@ const Header: React.FC = () => {
       >
         <ShoppingCart className="h-6 w-6" />
         {getItemCount() > 0 && (
-          <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full h-6 w-6 flex items-center justify-center font-bold">
+          <span className="absolute -top-5 -right-2 bg-red-500 text-white text-xs rounded-full h-6 w-6 flex items-center justify-center font-bold">
             {getItemCount()}
           </span>
         )}
