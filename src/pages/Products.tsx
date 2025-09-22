5432139ae8ca8762d98e3c64ec5b738a341dae65
Kayky Nery <kaykynery38@gmail.com>
2025-09-22 19:37:18 -0300

    Updated Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -489,11 +489,11 @@ const Products: React.FC = () => {
       name: 'Estaca 04 á 06 2,20 MT',
       description: 'Estaca de madeira 04 á 06 de diâmetro com 2,20 M',
       image:
-      '     '
+      '     ',
       price: 7.99,
       weight: 10,
       category: 'Madeiras'
-    },
+    }
     
   ];
 
