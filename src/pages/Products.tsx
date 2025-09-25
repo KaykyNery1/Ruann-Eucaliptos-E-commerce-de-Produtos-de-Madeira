d340381b9e1267d66be2365a25772f13c57daca2
Kayky Nery <kaykynery38@gmail.com>
2025-09-25 15:27:13 -0300

    Updated Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -494,16 +494,7 @@ const Products: React.FC = () => {
       category: 'Madeiras'
     }
 
-    {
-      id: '53',
-      name: 'Estaca 06/08 2,20 MT',
-      description: 'Estaca de madeira 06 á 08 de diâmetro com 2,20 M',
-      image:
-      'https://armazemdoeucalipto.com.br/wp-content/uploads/2023/10/Mourao-Poste-Troncode-e-Viga-eucalipto-tratado-3.webp',
-      price: 7.99,
-      weight: 10,
-      category: 'Madeiras'
-    }
+  
     
   ];
 
